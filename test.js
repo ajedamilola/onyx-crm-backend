719e12826839e025177158cbdff5b627af1044a0
ajeDamilola <ajedamilola2005@gmail.com>
2023-07-06 12:27:43 +0100

    Better Commit

--- test.js
+++
@@ -1,5 +0,0 @@
-// const { sendMail } = require("./functions");
-
-const { isInCurrentWeek } = require("./functions");
-
-// sendMail("damilola@telservenet.com","ajedamilola2005@gmail.com","Hello","This is a test message");
